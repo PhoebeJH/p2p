025574a39773de72e6492ddfc924cbfa10013c92
xiaoliaimaomi <3245498257@qq.com>
2019-05-28 18:35:11 +0800

    111

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,10 @@
   </div>
 </template>
 
+<link type="text/css" rel="stylesheet" href="./static/layui/css/layui.css"/> 
+    <script type="text/javascript" src="./static/layui/layui.js"></script>
 <script>
+	
   import NavTop from "./components/nav/NavTop.vue"
   import Content from "./components/Content.vue"
   export default {
